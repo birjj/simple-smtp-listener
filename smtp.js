4b94352f256ab6cdcc5cd652d0fb6912ba4515cc
birjolaxew <johanringmann@gmail.com>
2017-05-06 04:34:26 +0200

    Added emitting & port parameter

--- smtp.js
+++ smtp.js
@@ -4,14 +4,21 @@ let parseMail = require("mailparser").simpleParser;
 let logger = require("winston");
 logger.cli();
 
+/**
+ * Acts as a basic listener SMTP server, listening for all emails.
+ * Emits events when a mail is received.
+ * Events:
+ *   email@domain => (mail)
+ *   err => (Error)
+ */
 module.exports = class SMTP extends EventEmitter {
-    constructor() {
+    constructor(port=25) {
         super();
         this.server = new SMTPServer({
             authOptional: true,
             onData: this.handleData.bind(this)
         });
-        this.server.listen(587);
+        this.server.listen(port);
         this.server.on("error", this.logErr);
     }
     destroy(cb) {
@@ -26,10 +33,13 @@ module.exports = class SMTP extends EventEmitter {
     }
     handleEmail(err, email) {
         if (err) {
+            this.emit("err",err);
             return logger.error(`[SMTP] Got malformed email: ${err}`);
         }
 
-        logger.info("Got e-mail:");
-        logger.info(email);
+        let targets = email.to.value;
+        for (var i = 0; i < targets.length; ++i) {
+            this.emit(targets[i].address, email);
+        }
     }
 };
