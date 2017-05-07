99e99ca334eff70fa44a94e236c710f6d82d62f7
birjolaxew <johanringmann@gmail.com>
2017-05-07 17:51:26 +0200

    Added RabbitMQ support

--- smtp.js
+++ smtp.js
@@ -1,12 +1,15 @@
 let EventEmitter = require("events").EventEmitter;
 let SMTPServer = require("smtp-server").SMTPServer;
 let parseMail = require("mailparser").simpleParser;
+let amqp = require("node-amqp");
 let logger = require("winston");
 logger.cli();
 
 /**
  * Acts as a basic listener SMTP server, listening for all emails.
  * Emits events when a mail is received.
+ * Can also publish events to RabbitMQ. This lets multiple applications listen in.
+ * RabbitMQ messages are published to queues with the same name as the received email
  * Events:
  *   email@domain => (mail)
  *   err => (Error)
@@ -15,7 +18,17 @@ logger.cli();
  *   Mail.on("test@example.com", mail=>{ ... });
  */
 module.exports = class SMTP extends EventEmitter {
-    constructor(port=25) {
+    /**
+     * 
+     * @param {Number} port The port to listen on
+     * @param {Object} amqp An optional object detailing RabbitMQ related options
+     *                      {
+     *                          host: "<url>",
+     *                          exchange: "<name>",
+     *                          ... // additional options for amqp.createConnection
+     *                      }
+     */
+    constructor(port=2525, amqp) {
         logger.debug(`[SMTP] Starting on :${port}`);
         super();
         this.server = new SMTPServer({
@@ -24,6 +37,10 @@ module.exports = class SMTP extends EventEmitter {
         });
         this.server.listen(port);
         this.server.on("error", this.handleErr);
+
+        if (amqp) {
+            this.setupAMQP(amqp);
+        }
     }
     destroy(cb) {
         logged.debug(`[SMTP] Destroying`);
@@ -32,6 +49,31 @@ module.exports = class SMTP extends EventEmitter {
     handleErr(err) {
         this.emit(err);
         logger.error(`[SMTP] Error: ${err}`);
+    }
+
+    setupAMQP(options) {
+        let exchange = options.exchange;
+        delete options.exchange;
+        this.amqp = amqp.createConnection(options);
+        
+        this.amqp.on("error", this.handleErr.bind(this));
+        this.amqp.on("ready", ()=>{
+            this.exchange = this.amqp.exchange(
+                exchange,
+                {
+                    type: "direct",
+                    autoDelete: false
+                });
+        });
+    }
+
+    emit(key, data) {
+        super.emit(key,data);
+
+        // publish to amqp if we should
+        if (this.exchange) {
+            this.exchange.publish(key,data);
+        }
     }
 
     handleData(stream, session, cb) {
--- test.js
+++ test.js
@@ -1,3 +1,19 @@
+let amqp = require("node-amqp");
 let SMTP = require("./smtp.js");
-let Mail = new SMTP();
-Mail.on("test@steam.jfagerberg.me", mail=>console.log(mail));
+let Mail = new SMTP(2525, {
+    host: "amqp://localhost",
+    exchange: "emails"
+});
+Mail.on("test@steam.jfagerberg.me", mail=>{
+    console.log("Got mail from EventEmitter:",mail);
+});
+
+let conn = amqp.createConnection({host: "amqp://localhost"});
+conn.on("ready", ()=>{
+    var q = conn.queue("test@steam.jfagerberg.me", ()=>{
+        q.subscribe((message,headers,deliverInfo,messageObj)=>{
+            console.log("Got mail from RabbitMQ:",message);
+            messageObj.acknowledge(false);
+        });
+    });
+});
