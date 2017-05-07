388f209e26d499b377aa185daed0949732665928
birjolaxew <johanringmann@gmail.com>
2017-05-07 19:36:23 +0200

    Fixed test

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 let amqp = require("amqp");
 let SMTP = require("./smtp.js");
 let Mail = new SMTP(2525, {
-    host: "amqp://localhost",
+    host: "localhost",
     exchange: "emails"
 });
 Mail.on("test@steam.jfagerberg.me", mail=>{
     console.log("Got mail from EventEmitter:",mail);
 });
 
-let conn = amqp.createConnection({host: "amqp://localhost"});
+let conn = amqp.createConnection({host: "localhost"});
 conn.on("ready", ()=>{
-    var q = conn.queue("test@steam.jfagerberg.me", ()=>{
+    var q = conn.queue("test", ()=>{
         q.subscribe((message,headers,deliverInfo,messageObj)=>{
             console.log("Got mail from RabbitMQ:",message);
             messageObj.acknowledge(false);
         });
+        q.bind("emails", "test@steam.jfagerberg.me");
     });
 });
